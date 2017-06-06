814cd5502764c671c2442ac200f51fecbab400bf
heng9188 <hengji9188@esri.com>
2017-06-05 17:34:55 -0700

    update

--- src/main/scala/esri/App.scala
+++ src/main/scala/esri/App.scala
@@ -1,5 +1,7 @@
 package esri
 
+import java.io.File
+
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
@@ -19,14 +21,15 @@ import org.apache.spark.sql.types.{LongType, StructField, StructType}
 object App {
 
     val conf = new SparkConf()
-        .setAppName("app")
-        .setMaster("local[2]")
+      .setAppName("app")
+      .setMaster("local[2]")
     val sc = new SparkContext(conf)
 
     val spark = SparkSession.builder()
-        .config("spark.sql.warehouse.dir", "file:///C:/Users/heng9188/IdeaProjects/spark_project/spark-warehouse")
-        .appName("app")
-        .getOrCreate()
+      .config("spark.sql.warehouse.dir", "file:///C:/Users/heng9188/IdeaProjects/spark_project/spark-warehouse")
+      .appName("app")
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
 
     // add index of columns to dataframe
     def addColumnIndex(df: DataFrame) = spark.createDataFrame(
@@ -45,40 +48,47 @@ object App {
 
         val path = "complete1.csv"
         val df = spark.read
-            .format("csv")
-            .option("header", true)
-            .csv(path)
+          .format("csv")
+          .option("header", true)
+          .csv(path)
 
         // data cleaning
         val data = df
-            .selectExpr(
-                "cast(datetime as string) datetime",
-                "cast(shape as string) shape",
-                "cast(duration as double) duration",
-                "cast(latitude as double) latitude",
-                "cast(longitude as double) longitude")
+          .selectExpr(
+              "cast(datetime as string) datetime",
+              "cast(shape as string) shape",
+              "cast(duration as double) duration",
+              "cast(latitude as double) latitude",
+              "cast(longitude as double) longitude")
 
         // parse datetime to timestamp
         val ts = unix_timestamp($"datetime", "MM/dd/yyyy HH:mm").cast("timestamp")
         val dataToParse = data
-            .withColumn("ts", ts)
-            .na
-            .drop()
-            .filter("latitude != 0.0")
-            .filter("duration != 0.0")
-            .select("ts", "shape", "duration", "latitude", "longitude")
-            .cache()
+          .withColumn("ts", ts)
+          .na
+          .drop()
+          .filter("latitude != 0.0")
+          .filter("duration != 0.0")
+          .select("ts", "shape", "duration", "latitude", "longitude")
+        dataToParse.cache()
 
+        //        dataToParse
+        //          .coalesce(1)
+        //          .select("latitude", "longitude")
+        //          .write
+        //          .format("csv")
+        //          .option("header", "true")
+        //          .save("hhha.csv")
         val tsArr = dataToParse.select("ts").rdd.collect().map(row => row(0))
 
         val arr1 = (tsArr(0) +: tsArr.slice(0, tsArr.length - 1))
-            .map(row => row.toString)
+          .map(row => row.toString)
 
         val arr2 = tsArr.map(row => row.toString)
         val timeDiff = sc.parallelize(arr1.zip(arr2))
-            .toDF("col1", "col2")
-            .withColumn("col1", $"col1".cast("timestamp"))
-            .withColumn("col2", $"col2".cast("timestamp"))
+          .toDF("col1", "col2")
+          .withColumn("col1", $"col1".cast("timestamp"))
+          .withColumn("col2", $"col2".cast("timestamp"))
         val diffSecs = col("col2").cast("long") - col("col1").cast("long")
 
 
@@ -86,38 +96,35 @@ object App {
         val df1WithIndex = addColumnIndex(dataToParse)
         val df2WithIndex = addColumnIndex(df2)
         val parsedData = df1WithIndex
-            .join(df2WithIndex, Seq("columnIndex"))
-            .drop("columnIndex", "ts", "col1", "col2")
+          .join(df2WithIndex, Seq("columnIndex"))
+          .drop("columnIndex", "ts", "col1", "col2")
 
         // assemble longitude and latitude to features
         val assembler = new VectorAssembler()
-            .setInputCols(Array("latitude", "longitude"))
-            .setOutputCol("features")
+          .setInputCols(Array("latitude", "longitude"))
+          .setOutputCol("features")
 
         // geoData
         val geoData = assembler
-            .transform(parsedData)
-            .cache()
+          .transform(parsedData)
+          .cache()
 
 
         val (labeled, clusters) = kMeans.kMeansCluster(geoData)
-//
-//        labeled.show()
-//        sys.exit()
         // transforming categorical data to numeric
         val indexer = new StringIndexer()
-            .setInputCol("shape")
-            .setOutputCol("shapeIndex")
-            .fit(labeled)
+          .setInputCol("shape")
+          .setOutputCol("shapeIndex")
+          .fit(labeled)
 
         val indexed = indexer.transform(labeled)
 
         // normalizing duration to unit mean, zero deviation
         val scaler = new StandardScaler()
-            .setInputCol("duration")
-            .setOutputCol("durations")
-            .setWithStd(true)
-            .setWithMean(false)
+          .setInputCol("duration")
+          .setOutputCol("durations")
+          .setWithStd(true)
+          .setWithMean(false)
 
         val toVec = udf[Vector, Double] {
             try {
@@ -137,65 +144,70 @@ object App {
 
 
         val vecData = indexed
-            .withColumn("duration", toVec(indexed("duration")))
-            .withColumn("diffMins", toVec(indexed("diffMins")))
+          .withColumn("duration", toVec(indexed("duration")))
+          .withColumn("diffMins", toVec(indexed("diffMins")))
 
         val scalerModel = scaler.fit(vecData)
         val scaledData1 = scalerModel.transform(vecData)
         val transformedData1 = scaledData1
-            .withColumn("duration", vecToDouble(scaledData1("durations")))
+          .withColumn("duration", vecToDouble(scaledData1("durations")))
 
         scaler
-            .setInputCol("diffMins")
-            .setOutputCol("scaledDiff")
-            .setWithStd(true)
-            .setWithMean(false)
+          .setInputCol("diffMins")
+          .setOutputCol("scaledDiff")
+          .setWithStd(true)
+          .setWithMean(false)
         val scalerModel2 = scaler.fit(transformedData1)
         val scaledData2 = scalerModel2.transform(transformedData1)
         val transformedData2 = scaledData2
-            .withColumn("diffMins", vecToDouble(scaledData2("scaledDiff")))
+          .withColumn("diffMins", vecToDouble(scaledData2("scaledDiff")))
 
         val assembler2 = new VectorAssembler()
-            .setInputCols(Array("shapeIndex", "duration", "diffMins"))
-            .setOutputCol("features")
+          .setInputCols(Array("shapeIndex", "duration", "diffMins"))
+          .setOutputCol("features")
 
         val trainingData = assembler2
-            .transform(transformedData2)
-            .select("features", "label")
-            .withColumn("label", $"label".cast("double"))
-
+          .transform(transformedData2)
+          .select("features", "label")
+          .withColumn("label", $"label".cast("double"))
         val splits = trainingData.randomSplit(Array(0.6, 0.4))
         val train = splits(0)
         val test = splits(1)
         val layers = Array[Int](3, 30, 30, clusters)
 
         val trainer = new MultilayerPerceptronClassifier()
-            .setLayers(layers)
-            .setBlockSize(128)
-            .setSeed(1234L)
-            .setMaxIter(100)
+          .setLayers(layers)
+          .setBlockSize(128)
+          .setSeed(1234L)
+          .setMaxIter(100)
 
         val pipeline = new Pipeline()
-            .setStages(Array(trainer))
+          .setStages(Array(trainer))
 
         //        train.show()
         //        sys.exit()
 
         val evaluator = new MulticlassClassificationEvaluator()
-            .setMetricName("accuracy")
+          .setMetricName("accuracy")
 
         val paramGrid = new ParamGridBuilder().build()
 
         val cv = new CrossValidator()
-            .setEstimator(pipeline)
-            .setEvaluator(evaluator)
-            .setEstimatorParamMaps(paramGrid)
-            .setNumFolds(5)
+          .setEstimator(pipeline)
+          .setEvaluator(evaluator)
+          .setEstimatorParamMaps(paramGrid)
+          .setNumFolds(5)
 
         val model1 = cv.fit(train)
         val results = model1.transform(test)
         val predictionAndLabels = results.select("prediction", "label")
-        results.show()
+        results
+          .select("prediction", "label")
+          .coalesce(1)
+          .write
+          .format("csv")
+          .option("header", "true")
+          .save("res.csv")
 
 
         println("Test set accuracy = " + evaluator.evaluate(predictionAndLabels))
--- src/main/scala/esri/kMeans.scala
+++ src/main/scala/esri/kMeans.scala
@@ -1,9 +1,10 @@
 package esri
 
-import esri.App.{spark, sc}
+import esri.App.{sc, spark}
 import spark.implicits._
 import org.apache.spark.ml.clustering.{KMeans, KMeansModel}
 import org.apache.spark.ml.linalg.Vector
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql
 import org.apache.spark.sql.functions._
 
@@ -13,11 +14,6 @@ import scala.util.control.Breaks.{break, breakable}
   * Created by heng9188 on 6/2/2017.
   */
 object kMeans {
-    // kmeans clustering algorithm
-    var model: KMeansModel = null
-    val kmeans = new KMeans()
-    // score of datapoints to centroid, the less the more accurate
-
 
     // transform vector to array
     val toArray = udf[Array[String], Vector] {
@@ -41,7 +37,7 @@ object kMeans {
     def toArrayDouble(x: Array[(Double, Int)], y: Int): Array[Double] = {
         try {
             x.filter(row => row._2 % 2 == y)
-                .map(row => row._1)
+              .map(row => row._1)
         } catch {
             case e: Exception => throw new Exception("\n" + "tooArrayDouble failed")
         }
@@ -50,73 +46,83 @@ object kMeans {
     def dataframeToArray(x: sql.DataFrame, inputColumn: String, outputColumn: String): Array[Double] = {
         try {
             x.withColumn(outputColumn, toDouble(x(inputColumn)))
-                .rdd.collect()
-                .map(row => row.get(0)
-                    .toString
-                    .toDouble)
+              .rdd.collect()
+              .map(row => row.get(0)
+                .toString
+                .toDouble)
         } catch {
             case e: Exception => throw new Exception("\n" + "dataframeToArray failed")
         }
     }
 
 
-    def clusteringScore(data: sql.DataFrame, k: Int): (Double, sql.DataFrame) = {
+    def clusteringScore(data: sql.DataFrame, k: Int): (Double, sql.DataFrame, sql.DataFrame, KMeansModel) = {
 
-        kmeans.setK(k).setSeed(1L)
-        model = kmeans.fit(data)
+        val kmeans = new KMeans().setK(k).setMaxIter(40)
+        //        data.show()
+        val model = kmeans.fit(data)
+
 
         // map cluster to numeric value, d is numeric value while c is the coordinates
         val clusterWithIndex = model.clusterCenters.zipWithIndex.map {
             case (c, d) => (d, c.toArray)
         }
-
-        // predict numerical clusters, and transform clusterWithIndex to DataFrame (originally it was array)
-        val prediction = model.transform(data)
-        // change the type of features in prediction from vector to array for future usage
-        val predictions = prediction
-            .withColumn("features", toArray(prediction("features")))
-        // transform clusterWithIndex to DataFrame, prediction contains the numerical value while centroid contains the coordinates
         val clusterCentroid = sc.parallelize(clusterWithIndex)
-            .toDF("label", "centroid")
+          .toDF("label", "centroid")
 
-        // pairing the original coordinates with its corresponding centroid
-        val combined = predictions.as("d1")
-            .join(clusterCentroid.as("d2"), col("d1.prediction") === col("d2.label"), "inner")
-            .drop(col("d1.prediction"))
+        // predict numerical clusters
+        val prediction = model.transform(data)
+        val result = prediction
+          .withColumn("label", prediction("prediction"))
+          .drop("features")
 
-        // extracting the coordinates
-        val originalLatLon = combined.selectExpr("explode(features) as e")
-        val clusterLatLon = combined.selectExpr("explode(centroid) as e")
+        // compute Euclidean distance
+        val err = math.sqrt(model.computeCost(data) / data.count())
+        println(err)
+        (err, result, clusterCentroid.select("centroid"), model)
+        //
 
-        // paring lat and long
-        val extractedOri = dataframeToArray(originalLatLon, "e", "features")
-            .zipWithIndex
+        //        // pairing the original coordinates with its corresponding centroid
+        //        val combined = predictions.as("d1")
+        //          .join(clusterCentroid.as("d2"), col("d1.prediction") === col("d2.label"), "inner")
+        //          .drop(col("d1.prediction"))
+        //
+        //        // extracting the coordinates
+        //
+        //        val originalLatLon = combined.selectExpr("explode(features) as e")
+        //        val clusterLatLon = combined.selectExpr("explode(centroid) as e")
+        //
+        //        // paring lat and long
+        //        val extractedOri = dataframeToArray(originalLatLon, "e", "features")
+        //          .zipWithIndex
+        //
+        //        val extractedClust = dataframeToArray(clusterLatLon, "e", "centroid")
+        //          .zipWithIndex
+        //
+        //        case class Row(i: Double, j: Double, k: Double, m: Double)
+        //        val latCluster = toArrayDouble(extractedClust, 0)
+        //        val longCluster = toArrayDouble(extractedClust, 1)
+        //        val lat = toArrayDouble(extractedOri, 0)
+        //        val long = toArrayDouble(extractedOri, 1)
+        //
+        //        // transforming the four arrays to four DataFrame columns
+        //        def zip4[A, B, C, D](l1: Array[A], l2: Array[B], l3: Array[C], l4: Array[D]) =
+        //            l1.zip(l2).zip(l3).zip(l4).map {
+        //                case (((a, b), c), d) => (a, b, c, d)
+        //            }
+        //
+        //        val xs = zip4(latCluster, longCluster, lat, long)
+        //        val tmp = sc.parallelize(xs).toDF("latCluster", "longCluster", "lat", "long")
+        //
+        //        // calculating the Euclidean distance
+        //        val res = tmp.select($"latCluster" - $"lat" as "d1", $"longCluster" - $"long" as "d2")
+        //        val square = res.select($"d1" * $"d1" as "d1", $"d2" * $"d2" as "d2")
+        //        val sumation = square.select($"d1" + $"d2" as "d")
+        //
+        //        val average = sumation.select(avg($"d"))
+        //        println(model.computeCost(data)/data.count())
+        //        sys.exit()
 
-
-        val extractedClust = dataframeToArray(clusterLatLon, "e", "centroid")
-            .zipWithIndex
-
-        case class Row(i: Double, j: Double, k: Double, m: Double)
-        val latCluster = toArrayDouble(extractedClust, 0)
-        val longCluster = toArrayDouble(extractedClust, 1)
-        val lat = toArrayDouble(extractedOri, 0)
-        val long = toArrayDouble(extractedOri, 1)
-
-        // transforming the four arrays to four DataFrame columns
-        def zip4[A, B, C, D](l1: Array[A], l2: Array[B], l3: Array[C], l4: Array[D]) =
-            l1.zip(l2).zip(l3).zip(l4).map { case (((a, b), c), d) => (a, b, c, d) }
-
-        val xs = zip4(latCluster, longCluster, lat, long)
-        val tmp = sc.parallelize(xs).toDF("latCluster", "longCluster", "lat", "long")
-
-        // calculating the Euclidean distance
-        val res = tmp.select($"latCluster" - $"lat" as "d1", $"longCluster" - $"long" as "d2")
-        val square = res.select($"d1" * $"d1" as "d1", $"d2" * $"d2" as "d2")
-        val sumation = square.select($"d1" + $"d2" as "d")
-        sumation.show()
-        val average = sumation.agg(sum("d").cast("long"))
-        average.show()
-        (math.sqrt(average.first().getLong(0)), combined.select("shape", "duration", "label", "diffMins"))
     }
 
     var k: Int = 0
@@ -128,15 +134,31 @@ object kMeans {
         var result: sql.DataFrame = null
         breakable {
             for (i <- 2 to 50) {
-                val (m, b) = clusteringScore(geoData, i)
-                if (prev > m && (prev - m) / prev < diff) {
+                val (err, r, c, model) = clusteringScore(geoData, i)
+                if (prev > err && (prev - err) / prev < diff) {
                     k = i
-                    result = b
+                    result = r
+                    val zipped = model.clusterCenters.flatMap(_.toArray).zipWithIndex
+                    val lat = zipped.collect {
+                        case (value, i) if i % 2 == 0 => value
+                    }
+                    val long = zipped.collect {
+                        case (value, i) if i % 2 != 0 => value
+                    }
+                    val clusters = sc.parallelize(lat.zip(long)).toDF("latitude", "longitude")
+                    clusters
+                      .coalesce(1)
+                      .select("latitude", "longitude")
+                      .write
+                      .format("csv")
+                      .option("header", "true")
+                      .save("hh.csv")
                     break
                 }
-                prev = m
+                prev = err
             }
         }
+        sys.exit()
         (result, k)
     }
 }
